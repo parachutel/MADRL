f55edcb7510ea2c5750084b2233428b6b1d4be21
shengli <lisheng@stanford.edu>
2019-02-25 17:15:50 -0800

    Add test run file

---
+++ test.sh
@@ -0,0 +1,7 @@
+python3 runners/run_multiwalker.py rllab \
+    --control decentralized \
+    --policy_hidden 100,50,25 \
+    --n_iter 200 \
+    --n_walkers 2 \
+    --batch_size 24000 \
+    --curriculum lessons/multiwalker/env.yaml
