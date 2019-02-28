7bcc26883d2336a4ee8198464044ee9002d6e4dd
shengli <lisheng@stanford.edu>
2019-02-28 15:24:09 -0800

    update

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@ export PYTHONPATH=$(pwd):$(pwd)/rltools:$(pwd)/rllab:$PYTHONPATH
 python3 runners/run_multiaircraft.py rllab \
     --control decentralized \
     --policy_hidden 100,50,25 \
-    --n_iter 200 \
+    --n_iter 2 \
     --n_agents 5 \
     --batch_size 24000 \
     --curriculum lessons/cas/env.yaml
