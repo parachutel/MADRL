5a953dd6edb5f1f0f09740095c7b0fce9e76d59f
shengli <lisheng@stanford.edu>
2019-02-28 18:55:50 -0800

    change train settings

--- test.sh
+++ test.sh
@@ -1,10 +1,12 @@
 export PYTHONPATH=$(pwd):$(pwd)/rltools:$(pwd)/rllab:$PYTHONPATH
 python3 runners/run_multiaircraft.py rllab \
     --control decentralized \
+    --sampler parallel \
     --policy_hidden 100,50,25 \
-    --n_iter 50 \
+    --n_iter 35 \
     --n_agents 15 \
     --batch_size 24000 \
+    --max_traj_len 5000 \
     --rew_arrival 15.0 \
     --rew_closing 2.5 \
     --rew_nmac -15.0 \
