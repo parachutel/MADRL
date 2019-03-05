995a0e3eea2c243154e6b65ae692da24a1b9c78c
shengli <lisheng@stanford.edu>
2019-03-05 11:16:59 -0800

    load test

--- load_test.py
+++ load_test.py
@@ -6,11 +6,11 @@ import json
 data_file_path = './rllab/data/experiment_2019_03_05_09_54_11_296673_PST_08c0f/itr_4.pkl'
 
 with tf.Session() as sess:
-	data = joblib.load(data_file_path)
+    data = joblib.load(data_file_path)
     policy = data['policy']
     env = data['env']
     path = rollout(env, policy, max_path_length=1000, 
-    				animated=False, speedup=1)
+                    animated=False, speedup=1)
 
 
 # run_experiment
