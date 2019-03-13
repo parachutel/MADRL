ede2659c07256eeb273f612d2127c0712ea64b4e
Sheng Li <lisheng@stanford.edu>
2019-03-13 13:40:59 -0700

    new train

--- train/train_trpo.sh
+++ train/train_trpo.sh
@@ -1,6 +1,6 @@
 export PYTHONPATH=$(pwd)/..:$(pwd)/../rltools:$(pwd)/../rllab:$PYTHONPATH
 python3 ../runners/run_multiaircraft.py rllab \
-    --exp_name trpo_full_curr_3_passes_ALL_ENV_Tmax_300_PEN_HEAVY_True_rew_nmac_-50_rew_arr_2\
+    --exp_name trpo_full_curr_3_passes_ALL_ENV_Tmax_300_PEN_HEAVY_True_rew_nmac_-150_rew_arr_2\
 	--algo tftrpo \
     --step_size 0.01 \
     --discount 0.99 \
@@ -13,7 +13,7 @@ python3 ../runners/run_multiaircraft.py rllab \
     --pen_action_heavy True \
     --rew_arrival 2.0 \
     --rew_closing 0.05 \
-    --rew_nmac -50.0 \
+    --rew_nmac -150.0 \
     --rew_large_turnrate -0.05 \
     --rew_large_acc -0.05 \
     --curriculum ../lessons/cas/env.yaml
