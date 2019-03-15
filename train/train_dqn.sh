e7e489ff83ec99f8909000819f4d5d3be010a0b5
shengli <lisheng@stanford.edu>
2019-03-15 16:28:50 -0700

    debug

--- train/train_dqn.sh
+++ train/train_dqn.sh
@@ -2,7 +2,7 @@ export PYTHONPATH=$(pwd)/..:$(pwd)/../rltools:$(pwd)/../rllab:$PYTHONPATH
 python3  -W ignore ../runners/train_dqn_disc_multi_aircraft.py \
     --n_iter 4000000 \
     --target_update_step 5000 \
-    --batch_size default=64 \
+    --batch_size 64 \
     --discount 0.99 \
     --max_experience_size 20000 \
     --traj_sim_len 300 \
