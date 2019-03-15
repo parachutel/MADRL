40b2484f5d1a6f5d8382ce376a6e6270e5f8ae02
Sheng Li <lisheng@stanford.edu>
2019-03-15 15:47:51 -0700

    train script update

--- train/train_trpo.sh
+++ train/train_trpo.sh
@@ -1,13 +1,13 @@
 export PYTHONPATH=$(pwd)/..:$(pwd)/../rltools:$(pwd)/../rllab:$PYTHONPATH
 python3 ../runners/run_multiaircraft.py rllab \
-    --exp_name trpo_20_agent_direct_ALL_ENV_Tmax_300_PEN_HEAVY_True_rew_nmac_-150_rew_arr_2\
+    --exp_name trpo_shift_ALL_ENV_Tmax_300_PEN_HEAVY_True_rew_nmac_-150_rew_arr_2\
 	--algo tftrpo \
     --step_size 0.01 \
     --discount 0.99 \
     --control decentralized \
     --sampler simple \
     --policy_hidden 100,50,50 \
-    --n_iter 200 \
+    --n_iter 1 \
     --batch_size 30000 \
     --max_path_length 1000 \
     --pen_action_heavy True \
