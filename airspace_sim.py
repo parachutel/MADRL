5df849afceb9ca799696c05e0ab1be611c0a84bb
shengli <lisheng@stanford.edu>
2019-03-15 15:38:52 -0700

    new viz

--- airspace_sim.py
+++ airspace_sim.py
@@ -11,13 +11,13 @@ import tensorflow as tf
 import numpy as np
 
 # data_file_path = './rllab/data/test/itr_399_Mar5_work_for_circle.pkl' # 399 Mar 5 works for circle
-data_file_path = './rllab/data/test/itr_299-50.pkl'
+data_file_path = './rllab/data/test/all_env/itr_2064_direct_real_good.pkl'
 
 tf.reset_default_graph()
 with tf.Session() as sess:
 	data = joblib.load(data_file_path)
 	policy = data['policy']
-	env = MultiAircraftEnv(n_agents=10, render_option=True)
+	env = MultiAircraftEnv(n_agents=15, render_option=True)
 	env.reset()
 
 	done = False
